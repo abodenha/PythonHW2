868a6201469ef54d96208b6e68758efbbdf6ebb4
Albert Bodenhamer <albert@readers-indigestion.com>
2020-04-29 15:56:21 -0700

    Add project files.

---
+++ PythonHW2/PythonHW2.py
@@ -0,0 +1,28 @@
+# Dictionaries
+person = {"albert":"cool dude", "alex":"awesome student", "mom":"busy"}
+
+# Lists
+cars = ["Tesla", "Ferrari", "Porsche", "Yugo"]
+coffee = ["Starbucks", "Peets", "Death Wish", "Folgers"]
+
+# Sets (See https://www.datacamp.com/community/tutorials/data-structures-python#sets)
+my_pocket = {"keys", "lint", "phone", "wallet"}
+clothes = {"pants", "shirt", "hat", "shoes", "nose warmer", "wallet"}
+
+# List of Dictionaries
+people = [{"name":"Ralph", "job":"Nose picker", "address":"1234 5th St"},
+          {"name":"Julie", "job":"Mountain Eater", "address":"321 A Ave"},
+          {"name":"Ahmed", "job":"Car Customizer", "address":"543 Any Blvd"},
+          {"name":"Avni", "job":"Ruler of the Univers", "address":"1234 5th St"}]
+
+# Exercizes
+# #1 Create a dictionary. It can contain anything you want.
+
+# #2a Print 'True' if my_pocket contains "phone"
+# #2b Remove "phone" from my_pocket and do it again.
+# #2c Print out things that are in both my_pocket and clothes
+
+# #3a Insert a new person into people (you'll need to search the web for how to
+# do this. Look for "python add something to list"
+# #3b For every person in people print out their name and address
+# but not their job.
